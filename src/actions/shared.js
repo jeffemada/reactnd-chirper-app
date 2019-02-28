f64d97f18eca889d25f78ebd24a642cee99d9a51
Jefferson Madalena <jefferson.madalena@gmail.com>
2019-02-27 21:06:01 -0300

    Implementadas as ações de consulta inicial dos dados.

---
+++ src/actions/shared.js
@@ -0,0 +1,16 @@
+import { getInitialData } from '../utils/api';
+import { setAuthedUser } from './authedUser';
+import { receiveTweets } from './tweets';
+import { receiveUsers } from './users';
+
+export const AUTHED_ID = 'sarah_edo';
+
+export function handleInitialData() {
+  return (dispatch) => {
+    return getInitialData().then(({ tweets, users }) => {
+      dispatch(receiveTweets(tweets));
+      dispatch(receiveUsers(users));
+      dispatch(setAuthedUser(AUTHED_ID));
+    });
+  };
+}
