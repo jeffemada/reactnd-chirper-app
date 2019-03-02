948251b6cefd5913468f963e6466bdd30ec7c798
Jefferson Madalena <jefferson.madalena@gmail.com>
2019-03-02 16:27:28 -0300

    Adicionado o componente de loading.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,15 +2,18 @@ import { getInitialData } from '../utils/api';
 import { setAuthedUser } from './authedUser';
 import { receiveTweets } from './tweets';
 import { receiveUsers } from './users';
+import { showLoading, hideLoading } from 'react-redux-loading';
 
 export const AUTHED_ID = 'sarah_edo';
 
 export function handleInitialData() {
   return (dispatch) => {
+    dispatch(showLoading());
     return getInitialData().then(({ tweets, users }) => {
       dispatch(receiveTweets(tweets));
       dispatch(receiveUsers(users));
       dispatch(setAuthedUser(AUTHED_ID));
+      dispatch(hideLoading());
     });
   };
 }
